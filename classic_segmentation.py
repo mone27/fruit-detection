4bf711b4ff7a8dc322f2c3ab898f5d797b21afaf
Simone Massaro (mone27) <massaro.simone.it@gmail.com>
2019-01-28 19:17:43 +0100

    started refactoring: - created classic_segmentation.py - ImageSegmentation works for getting a clean mask - still need to clean the class API
    Signed-off-by: Simone Massaro (mone27) <massaro.simone.it@gmail.com>

---
+++ classic_segmentation.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from pathlib import Path
+
+
+class ImageSegmentation:
+    """A class for image segmentation usage:"""
+
+    def __init__(self, **kwargs):
+        """parameter:
+            filename (must be specified) path to the image file
+            threshold = 100
+            denoise = 3
+            min_area = 3
+            max_area = 50
+            """
+        self.image = cv2.imread(str(kwargs['filename']))
+        self.mask = self.image
+        self.denoise_size = kwargs.get('denoise', 3)
+        self.threshold = kwargs.get('threshold', 100)
+        width = self.image.shape[:2][1]
+        self.min_area = int(width * kwargs.get('max_area', 3) / 100) ** 2  # set default value
+        self.max_area = int(width * kwargs.get('min_area', 50) / 100) ** 2  # set default value
+
+    def get_mask(self):
+
+        self.mask = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        _, self.mask = cv2.threshold(self.mask, self.threshold, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        self.floodfill().denoise()
+        self.clean_mask()
+
+    def cut_image(self, cut_left, cut_right, cut_top, cut_bottom):
+        h, w = self.image.shape[:2]
+        w_left = int(w * cut_left / 100)
+        w_right = int(w * cut_right / 100)
+        h_top = int(h * cut_top / 100)
+        h_bottom = int(h * cut_bottom / 100)
+        self.image = self.image[h_top:h_bottom, w_left:w_right, :]
+
+    def floodfill(self):
+        mask_floodfill = self.mask.copy()
+        hf, wf = self.mask.shape[:2]
+        mask = np.zeros((hf + 2, wf + 2), np.uint8)
+        cv2.floodFill(mask_floodfill, mask, (0, 0), 255)
+        mask_floodfill_inv = cv2.bitwise_not(mask_floodfill)
+        self.mask = self.mask | mask_floodfill_inv
+        return self
+
+    def show(self):
+        cv2.namedWindow("image: ", cv2.WINDOW_NORMAL)
+        cv2.namedWindow("mask: ", cv2.WINDOW_GUI_NORMAL)
+        cv2.resizeWindow("mask: ", 1920, 1080)
+        cv2.imshow("image: ", self.image)
+        cv2.imshow("mask: ", self.mask)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def denoise(self):
+        kernel = np.ones((self.denoise_size, self.denoise_size), np.uint8)
+        self.mask = cv2.morphologyEx(self.mask, cv2.MORPH_OPEN, kernel, iterations=2)
+        return self
+
+    @staticmethod
+    def get_contours(image):
+        """just a wrapper around cv2.findContours"""
+        _, contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        return contours
+
+    def clean_mask(self):
+        contours = self.get_contours(self.mask)
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area < self.min_area or area > self.max_area:
+                x, y, w, h = cv2.boundingRect(contour)
+                cv2.rectangle(self.mask, (x, y), (x + w + 10, y + h + 10), 0, -1)
+
+
+mask = ImageSegmentation(filename=Path("Scan/albicocche1.tif"))
+mask.get_mask()
+mask.show()
