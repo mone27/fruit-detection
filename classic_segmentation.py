308e4ef6bf1361110486ad1b2011a2a2dfb3bc88
Simone Massaro (mone27) <simonemassaro@unitus.it>
2019-02-05 17:27:18 +0100

    deleted old dataset

--- classic_segmentation.py
+++ classic_segmentation.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-from pathlib import Path
+
+# Copyright
 
 
-class ImageSegmentation:
+class ClassicSegmentation:
     """A class for image segmentation usage:"""
 
     def __init__(self, **kwargs):
@@ -16,21 +16,24 @@ class ImageSegmentation:
             max_area = 50
             """
         self.image = cv2.imread(str(kwargs['filename']))
-        self.mask = self.image
+        self.mask = None
         self.denoise_size = kwargs.get('denoise', 3)
         self.threshold = kwargs.get('threshold', 100)
         width = self.image.shape[:2][1]
         self.min_area = int(width * kwargs.get('max_area', 3) / 100) ** 2  # set default value
         self.max_area = int(width * kwargs.get('min_area', 50) / 100) ** 2  # set default value
 
-    def get_mask(self):
+        self.individual_masks = []
+        self.individual_images = []
+        # can call get_mask() here...
 
+    def get_mask(self):
         self.mask = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         _, self.mask = cv2.threshold(self.mask, self.threshold, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        self.floodfill().denoise()
-        self.clean_mask()
+        self._floodfill()._denoise()
+        self._clean_mask()
 
-    def cut_image(self, cut_left, cut_right, cut_top, cut_bottom):
+    def _cut_image(self, cut_left, cut_right, cut_top, cut_bottom):
         h, w = self.image.shape[:2]
         w_left = int(w * cut_left / 100)
         w_right = int(w * cut_right / 100)
@@ -38,7 +41,7 @@ class ImageSegmentation:
         h_bottom = int(h * cut_bottom / 100)
         self.image = self.image[h_top:h_bottom, w_left:w_right, :]
 
-    def floodfill(self):
+    def _floodfill(self):
         mask_floodfill = self.mask.copy()
         hf, wf = self.mask.shape[:2]
         mask = np.zeros((hf + 2, wf + 2), np.uint8)
@@ -48,6 +51,7 @@ class ImageSegmentation:
         return self
 
     def show(self):
+        # can refactor to use
         cv2.namedWindow("image: ", cv2.WINDOW_NORMAL)
         cv2.namedWindow("mask: ", cv2.WINDOW_GUI_NORMAL)
         cv2.resizeWindow("mask: ", 1920, 1080)
@@ -56,7 +60,7 @@ class ImageSegmentation:
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def denoise(self):
+    def _denoise(self):
         kernel = np.ones((self.denoise_size, self.denoise_size), np.uint8)
         self.mask = cv2.morphologyEx(self.mask, cv2.MORPH_OPEN, kernel, iterations=2)
         return self
@@ -67,7 +71,7 @@ class ImageSegmentation:
         _, contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         return contours
 
-    def clean_mask(self):
+    def _clean_mask(self):
         contours = self.get_contours(self.mask)
         for contour in contours:
             area = cv2.contourArea(contour)
@@ -75,7 +79,46 @@ class ImageSegmentation:
                 x, y, w, h = cv2.boundingRect(contour)
                 cv2.rectangle(self.mask, (x, y), (x + w + 10, y + h + 10), 0, -1)
 
+    def _clean_image(self):
+        self.image = self.image & \
+                np.   stack([self.mask, self.mask, self.mask], axis=2) #to make background perfectly black
 
-mask = ImageSegmentation(filename=Path("Scan/albicocche1.tif"))
+    def _calculate_individual_masks(self):
+        if self.individual_masks:  # skip if already executed
+            return
+        self.get_mask()
+        self._clean_image()
+        contours = self.get_contours(self.mask)
+
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            image_section = self.image[y:y + h, x:x + w]
+            mask_section = self.mask[y:y + h, x:x + w]
+            self.individual_masks.append(image_section)
+            self.individual_images.append(mask_section)
+
+    @property
+    def small_masks(self):
+        self._calculate_individual_masks()
+        return self.individual_masks
+
+    @property
+    def small_images(self):
+        self._calculate_individual_masks()
+        return self.individual_images
+
+
+
+
+
+
+
+
+
+mask = ClassicSegmentation(filename="Scan/albicocche1.tif")
 mask.get_mask()
-mask.show()
+for i, m in zip(mask.small_masks, mask.small_images):
+    cv2.imshow("mask", m)
+    cv2.imshow("img", i)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
